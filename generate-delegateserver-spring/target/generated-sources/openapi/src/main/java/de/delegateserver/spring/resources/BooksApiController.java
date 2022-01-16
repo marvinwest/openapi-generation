d8c88c54388d540235c0e621fddf442bf34fc255
marvinwest <marvin.westphal@stud.uni-due.de>
2022-01-16 16:17:12 +0100

    replaces responseServer by delegateServer

---
+++ generate-delegateserver-spring/target/generated-sources/openapi/src/main/java/de/delegateserver/spring/resources/BooksApiController.java
@@ -0,0 +1,22 @@
+package de.delegateserver.spring.resources;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import java.util.Optional;
+@javax.annotation.Generated(value = "org.openapitools.codegen.languages.SpringCodegen", date = "2022-01-16T16:03:53.688844200+01:00[Europe/Berlin]")
+@Controller
+@RequestMapping("${openapi.bookstorage.base-path:}")
+public class BooksApiController implements BooksApi {
+
+    private final BooksApiDelegate delegate;
+
+    public BooksApiController(@org.springframework.beans.factory.annotation.Autowired(required = false) BooksApiDelegate delegate) {
+        this.delegate = Optional.ofNullable(delegate).orElse(new BooksApiDelegate() {});
+    }
+
+    @Override
+    public BooksApiDelegate getDelegate() {
+        return delegate;
+    }
+
+}
---
+++ generated-server-test/src/test/java/generated/server/book/DelegateServerIT.java
@@ -0,0 +1,66 @@
+package generated.server.book;
+
+import java.util.UUID;
+
+import javax.ws.rs.core.Response;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+@DisplayName("Book")
+public class DelegateServerIT extends SystemTest {
+
+	@Test
+	@DisplayName("Can be Created")
+	void post() {
+		// given
+		final var book = bookSystem.buildOrwell();
+
+		// when
+		Response response = bookSystem.tryStoringBook(book);
+
+		// then
+		bookSystem.getVerifier().verifyStatus(response, Response.Status.CREATED);
+	}
+	
+	@Test
+	@DisplayName("is found by bookId after creation")
+	void CreatedBookisFoundByBookId() {
+		// given
+		final var expectedBook = bookSystem.buildOrwell();
+		Response postResponse = bookSystem.tryStoringBook(expectedBook);
+		final var bookInformation = postResponse.readEntity(BookInformation.class);
+		
+		//when
+		final var bookId = UUID.fromString(bookInformation.bookId);
+		Response getResponse = bookSystem.tryFetchingBookbyBookId(bookId);
+		final var foundBook = getResponse.readEntity(Book.class);
+		
+		//then
+		bookSystem.bookVerifier.booksAreEqual(expectedBook, foundBook);
+	}
+
+	@Disabled("not functioning for now")
+	@Test
+	@DisplayName("is Found in BookInformation after Creation")
+	void createdBookIsFound() {
+		// given
+		final var book = bookSystem.buildOrwell();
+		Response postResponse = bookSystem.tryStoringBook(book);
+		final var bookInformation = postResponse.readEntity(BookInformation.class);
+
+		// when
+		Response getResponse = bookSystem.tryFetchingBooks();
+		final var bookInformationList = getResponse.readEntity(BookInformationList.class);
+		
+
+		// then
+		Assertions.assertAll(
+			() -> Assertions.assertEquals(book.title, bookInformation.title),
+			() -> bookSystem.bookVerifier.assertBookInformationInBookInformationList(bookInformation, bookInformationList)
+		);
+	}
+
+}
